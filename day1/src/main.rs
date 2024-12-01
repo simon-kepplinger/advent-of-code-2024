68b032dd7c8142f3be16b437d7efe83462ea754d
Simon Kepplinger <simon.kepplinger@valcome.at>
2024-12-01 19:35:56 +0100

    day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,35 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("in/input")
-        .expect("could not open file in/input");
+    let values: Vec<u32> = read("in/input")
+        .split_whitespace()
+        .map(|s| s.parse().expect("Parse error"))
+        .collect();
 
-    println!("{}", content);
+    let (left, right): (Vec<_>, Vec<_>) = values
+        .into_iter()
+        .enumerate()
+        .partition(|(i, _)| i % 2 == 0);
+
+    let mut count_map = HashMap::new();
+    
+    for (_, v) in right {
+        let count = count_map.entry(v).or_insert(0);
+        *count += 1;
+    }
+
+    let mut sum: u32 = 0;
+
+    for (_, v) in left  {
+        let count = count_map.get(&v).unwrap_or(&0);
+        sum += count * v;
+    }
+
+    println!("{}", sum);
+}
+
+fn read(path: &str) -> String {
+    fs::read_to_string(path)
+        .expect(&format!("could not open file {}", path))
 }
