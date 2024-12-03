73b8baa11b2ebaec79da97a19216e5990eba7ab3
Simon Kepplinger <simon.kepplinger@valcome.at>
2024-12-03 21:32:08 +0100

    day2

---
+++ day2/src/part1.rs
@@ -0,0 +1,38 @@
+use std::fs;
+
+fn main() {
+    let sum: u32 = read("in/input")
+        .lines()
+        .map(|l| {
+            l.split(" ")
+                .map(|s| s.parse().unwrap())
+                .collect()
+        })
+        .map(|v| is_secure(v))
+        .sum();
+
+    println!("{:?}", sum);
+}
+
+fn is_secure(levels: Vec<i32>) -> u32 {
+    let mut diff= Vec::with_capacity(levels.len() - 1);
+
+    for i in 0..levels.len() - 1 {
+        diff.push(levels[i] - levels[i + 1]);
+    }
+
+    let is_full_pos = diff.iter().all(|n| *n < 0);
+    let is_full_neg = diff.iter().all(|n| *n > 0);
+    let is_in_range = diff.iter().all(|n| 1 <= n.abs() && n.abs() <= 3);
+
+    if is_in_range && (is_full_pos || is_full_neg) {
+        1
+    } else {
+        0
+    }
+}
+
+fn read(path: &str) -> String {
+    fs::read_to_string(path)
+        .expect(&format!("could not open file {}", path))
+}
